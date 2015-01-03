e92f745148fcb952492c3c72a01d40d4a7604d03
Eduardo Robles <eduardorobles@rgvlibrobox.com>
2015-01-03 16:52:11 -0600

    initial commit

---
+++ schema.sql
@@ -0,0 +1,11 @@
+drop table if exists Employees;
+
+CREATE TABLE Employees (
+fName text NOT NULL,
+lName text NOT NULL,
+title text NOT NUll,
+cell integer NOT NULL,
+primaryContactNum integer NOT NULL,
+secondaryContactNum integer NOT NULL,
+email text NOT NULL
+);
